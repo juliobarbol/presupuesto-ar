e615d202b45c45ff4c13d9ae6c7e74a9520631a8
juliobarbol <juliobarribolbo@gmail.com>
2026-06-05 21:08:00 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 //  Para forzar actualizacion tras un deploy: subir el CACHE_VERSION.
 // ============================================================
 
-const CACHE_VERSION = 'presupuesto-v9';
+const CACHE_VERSION = 'presupuesto-v10';
 const APP_SHELL = [
   './',
   './index.html',
